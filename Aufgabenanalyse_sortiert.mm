ce4271f4a4069cbfa01bae1dcedadd8ffc36dc02
Itschik <155478606+Itschik@users.noreply.github.com>
2025-07-29 15:14:52 +0200

    Add files via upload

---
+++ Aufgabenanalyse_sortiert.mm
@@ -0,0 +1,88 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1751971763597" ID="ID_1140411586" MODIFIED="1751989551041" TEXT="Analyse der Aufgabenstellung">
+<node CREATED="1751989552821" HGAP="22" ID="ID_335364814" MODIFIED="1751991362755" POSITION="left" TEXT="ToDo&#xb4;s" VSHIFT="-21">
+<icon BUILTIN="list"/>
+<node CREATED="1751988249800" HGAP="-43" ID="ID_157656493" MODIFIED="1752588539734" TEXT="Empfehlungen f&#xfc;r die Umsetzung der k&#xfc;nftigen Anforderungen erstellen" VSHIFT="-69">
+<icon BUILTIN="full-7"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751987965011" HGAP="79" ID="ID_61804246" MODIFIED="1752587590482" TEXT="Annahmen treffen/ Ideen finden f&#xfc;r k&#xfc;nftige Anforderungen an das Glossar" VSHIFT="-62">
+<icon BUILTIN="full-6"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1751973093516" ID="ID_371170671" MODIFIED="1751990229570" TEXT="Prototyp (als Basis) dient dazu, Umfang und Art der Begriffserl&#xe4;uterungen im Glossar abstimmen zu k&#xf6;nnen"/>
+</node>
+<node CREATED="1751988140242" HGAP="182" ID="ID_467642543" MODIFIED="1752587566863" TEXT="Prozess entwickeln zur Aktualisierung von bestehenden Inhalten oder Einf&#xfc;gen von neuen Inhalten" VSHIFT="-18">
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1752138983853" ID="ID_642486877" MODIFIED="1752178709151" TEXT="Anhand eines vorhandenen Begriffs beschreiben">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1752143395238" ID="ID_1819186447" MODIFIED="1752217484731" TEXT="Flussdiagramm erstellen">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1751972828605" HGAP="162" ID="ID_1078930976" MODIFIED="1752587603545" TEXT="Prototyp f&#xfc;r ein Online-Glossar mit relevanten Begriffsdefinitionen erstellen" VSHIFT="36">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1751973617195" ID="ID_1420767716" MODIFIED="1752088461862" TEXT="MUST: Gloassar in HTML erstellen, Formatierungen in CSS und JAVAScript f&#xfc;r Suchfunktion">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751973322163" ID="ID_410466050" MODIFIED="1752090783652" TEXT="MUST: 1 Startseite, 5 Unterseiten mit jeweils einer Begriffserl&#xe4;uterung">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751973110411" ID="ID_1570086811" MODIFIED="1752088474300" TEXT="MUST: Verlinkungen und zus&#xe4;tzliche Dokumente sollen bereitgestellt werden k&#xf6;nnen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751973682962" ID="ID_865784765" MODIFIED="1752088478531" TEXT="MUST: Datum und Uhrzeit der letzten &#xc4;nderung/Speicherung der jeweiligen HTML-Seite am Seitende anzeigen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751990867911" ID="ID_1165413879" MODIFIED="1752088483946" TEXT="MUST: Datum und Uhrzeit der letzten &#xc4;nderung der jeweiligen HTML-Seite speichern">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1752091409447" ID="ID_526700889" MODIFIED="1752139850263" TEXT="Prototypen vergleichen und Auswahl treffen">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1751973730145" HGAP="136" ID="ID_746800902" MODIFIED="1752221920528" TEXT="Handout erstellen f&#xfc;r Mitarbeiter um eigene Erweiterungen vornehmen zu k&#xf6;nnen; Dazu die Befehle erl&#xe4;utern (HTML, CSS und Javascript)" VSHIFT="47">
+<icon BUILTIN="full-5"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1751973270115" HGAP="-38" ID="ID_33205148" MODIFIED="1752587561180" TEXT=" 5 selbstgew&#xe4;hlte Begriffe aus dem Bereich Logistik, IT und Marketing suchen" VSHIFT="52">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1751992627257" ID="ID_1884828636" MODIFIED="1751992646111" TEXT="sinnvoll miteinander verlinkbar"/>
+<node CREATED="1751992648185" ID="ID_1070046058" MODIFIED="1751992657793" TEXT="einfach zu erkl&#xe4;ren"/>
+<node CREATED="1751992659896" ID="ID_665624476" MODIFIED="1751992751562" TEXT="m&#xf6;glichst eindeutig der IT, dem Marketing oder der Logistik zuzuordnen"/>
+<node CREATED="1751992963934" ID="ID_1284191963" MODIFIED="1751992976514" TEXT="am besten optisch gut darstellbar"/>
+<node CREATED="1751993629406" ID="ID_1715249738" MODIFIED="1751993658742" TEXT="am besten abteilungs&#xfc;bergreifende Verwendung mit hohem Misstverst&#xe4;ndnispotenzial"/>
+</node>
+<node CREATED="1752091242839" HGAP="-244" ID="ID_326713013" MODIFIED="1752587559547" TEXT="Geeignetes Konzept f&#xfc;r das Informationsdesign entwerfen" VSHIFT="20">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1752091327759" ID="ID_653414534" MODIFIED="1752138827786" TEXT="Startseite entwerfen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1752091389990" ID="ID_1540957853" MODIFIED="1752138834826" TEXT="Begriffseiten entwerfen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1752091398118" ID="ID_796423206" MODIFIED="1752138838783" TEXT="Wireframe erstellen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1752143846971" ID="ID_1992592287" MODIFIED="1752143857077" TEXT="Ordnerstruktur erstellen">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1751989404979" FOLDED="true" HGAP="-66" ID="ID_129390237" MODIFIED="1752589406663" POSITION="right" TEXT="Problem" VSHIFT="-86">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1751972789132" ID="ID_60448238" MODIFIED="1751972825019" TEXT="Unterschiedliche Interpretation der Begriffe betrifft Abteilungen IT, Marketing und Logistik"/>
+<node CREATED="1751972681005" ID="ID_933709038" MODIFIED="1751989483443" TEXT="Vielfalt an Fachbegriffen, die unterschiedlich interpretiert werden"/>
+<node CREATED="1752156738181" ID="ID_996794368" MODIFIED="1752156790994" TEXT="F&#xfc;r neue Mitarbeiter oder Dienstleister des Unternehmens ist die Vielfalt der Interpretationen eine gro&#xdf;e Schwierigkeit"/>
+</node>
+<node CREATED="1751989330791" FOLDED="true" HGAP="-11" ID="ID_513041346" MODIFIED="1752589403749" POSITION="right" TEXT="Allgemeine Informationen" VSHIFT="-96">
+<icon BUILTIN="info"/>
+<node CREATED="1751972642176" HGAP="68" ID="ID_1965964645" MODIFIED="1751989357804" TEXT="Log7-Consult GmbH, einem Beratungsunternehmen im Logistikumfeld" VSHIFT="-29"/>
+</node>
+</node>
+</map>
---
+++ Aufgabenanalyse_unsortiert.mm
@@ -0,0 +1,19 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1751971763597" ID="ID_1140411586" MODIFIED="1751973109317" TEXT="Analyse der Aufgabenstellung">
+<node CREATED="1751972642176" ID="ID_1965964645" MODIFIED="1751972669568" POSITION="right" TEXT="Log7-Consult GmbH, einem Beratungsunternehmen im Logistikumfeld"/>
+<node CREATED="1751972681005" ID="ID_933709038" MODIFIED="1751972702116" POSITION="left" TEXT="Vielfalt an Fachbegriffen, die unterschiedliche interpretiert werden"/>
+<node CREATED="1751972789132" ID="ID_60448238" MODIFIED="1751972825019" POSITION="right" TEXT="Unterschiedliche Interpretation der Begriffe betrifft Abteilungen IT, Marketing und Logistik"/>
+<node CREATED="1751972828605" ID="ID_1078930976" MODIFIED="1751972895516" POSITION="left" TEXT="Prototyp f&#xfc;r ein Online-Glossar mit relevanten Begriffsdefinitionen erstellen"/>
+<node CREATED="1751973093516" ID="ID_371170671" MODIFIED="1751973128888" POSITION="right" TEXT="MUST: Begriffserl&#xe4;uterungen im Glossar abstimmen"/>
+<node CREATED="1751973110411" ID="ID_1570086811" MODIFIED="1751973225947" POSITION="left" TEXT="MUST: Verlinkungen und zus&#xe4;tzliche Dokumente sollen bereitgestellt werden k&#xf6;nnen"/>
+<node CREATED="1751973270115" ID="ID_33205148" MODIFIED="1751973314523" POSITION="right" TEXT="MUST: 5 selbstgew&#xe4;hlte Begriffe aus dem Bereich Logistik, IT und Marketing suchen"/>
+<node CREATED="1751973322163" ID="ID_410466050" MODIFIED="1751973591764" POSITION="left" TEXT="MUST: 1 Startseite, 5 Unterseiten mit jeweils einer Begriffserl&#xe4;uterung"/>
+<node CREATED="1751973617195" ID="ID_1420767716" MODIFIED="1751973668624" POSITION="right" TEXT="MUST: Gloassar in HTML erstellen, Formatierungen in CSS und JAVAScript f&#xfc;r Suchfunktion"/>
+<node CREATED="1751973682962" ID="ID_865784765" MODIFIED="1751973723744" POSITION="left" TEXT="MUST: Datum und Uhrzeit der letzten Speicherung der jeweiligen HTML-Seite am Seitende anzeigen"/>
+<node CREATED="1751973730145" ID="ID_746800902" MODIFIED="1751987959608" POSITION="right" TEXT="MUST: Handout erstellen f&#xfc;r Mitarbeiter um eigene Erweiterungen vornehmen zu k&#xf6;nnen; Dazu die Befehle erl&#xe4;utern (HTML, CSS und Javascript)"/>
+<node CREATED="1751987965011" ID="ID_61804246" MODIFIED="1751988137714" POSITION="left" TEXT="Annahmen treffen/ Ideen finden f&#xfc;r die k&#xfc;nftige Anforderungen an das Glossar (Could und Would Anforderungen, keine MUST)"/>
+<node CREATED="1751988140242" ID="ID_467642543" MODIFIED="1751988215827" POSITION="right" TEXT="Prozess entwickeln zur Aktualisierung von bestehenden Inhalten oder Einf&#xfc;gen von neuen Inhalten"/>
+<node CREATED="1751988249800" ID="ID_157656493" MODIFIED="1751988291565" POSITION="left" TEXT="Empfehlungen f&#xfc;r die Umsetzung der Could und Would Anforderungen erstellen"/>
+</node>
+</map>
